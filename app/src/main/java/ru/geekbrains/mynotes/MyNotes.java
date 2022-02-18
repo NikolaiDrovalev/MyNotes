a74588c3a118091543bb5d6a52235de3240658fd
Nikolai <ndrovalev@gmail.com>
2022-02-18 15:53:27 +0300

    Домашнее задание 8. Добавил меню с иконками выхода из приложения и отображения версии приложения.

--- app/src/main/java/ru/geekbrains/mynotes/MyNotes.java
+++ app/src/main/java/ru/geekbrains/mynotes/MyNotes.java
@@ -1,7 +1,10 @@
 package ru.geekbrains.mynotes;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
@@ -16,6 +19,27 @@ public class MyNotes extends AppCompatActivity {
             MyNotesFragment myNotesFragment = MyNotesFragment.newInstance();
             getSupportFragmentManager().beginTransaction().replace(R.id.listOfNotes, myNotesFragment).commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_notes,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case (R.id.action_version):
+                getSupportFragmentManager().beginTransaction().replace(R.id.listOfNotes,new VersionFragment()).addToBackStack("").commit();
+                return true;
+            case (R.id.action_exit):
+                finish();
+                return true;
+
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
