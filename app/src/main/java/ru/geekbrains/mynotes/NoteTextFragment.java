ae6a4cdc3567f53d4ff7cf209355660520ea977c
Nikolai <ndrovalev@gmail.com>
2022-02-18 16:22:57 +0300

    Домашнее задание 8. Добавил меню в текст заметки с 2 Toast - "Добавить файл" и "Поделиться".

--- app/src/main/java/ru/geekbrains/mynotes/NoteTextFragment.java
+++ app/src/main/java/ru/geekbrains/mynotes/NoteTextFragment.java
@@ -4,10 +4,14 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -34,8 +38,29 @@ public class NoteTextFragment extends Fragment {
     }
 
     @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_note_text_fragment,menu);
+        menu.findItem(R.id.action_version).setVisible(false);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case (R.id.action_toast_add_file):
+                Toast.makeText(requireContext(),"Добавляется любой файл", Toast.LENGTH_LONG).show();
+                return true;
+            case (R.id.action_toast_will_share):
+                Toast.makeText(requireContext(),"Отправляем заметку в соцсети", Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setHasOptionsMenu(true);
         note = getArguments().getParcelable(ARG_NOTE);
         getChildFragmentManager().beginTransaction().replace(R.id.container_child, NoteTextChildFragment.newInstance(note)).addToBackStack("").commit();
         TextView textViewTwo = view.findViewById(R.id.textViewTwo);
